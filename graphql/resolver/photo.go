602e0b1881b1cb370ff65373906f5181922406f3
Kevin Hsu <hsukvn@gmail.com>
2018-10-09 15:16:58 +0800

    Refine file hierachy

--- graphql/resolver/photo.go
+++ graphql/resolver/photo.go
@@ -3,10 +3,12 @@ package resolver
 import (
 	"context"
 
+	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/wediin/curator/graphql/model"
 	"github.com/wediin/curator/lib/google"
 )
 
+// Query
 func (r *Resolver) Photos(ctx context.Context) (*[]*photoResolver, error) {
 	mediaItems, err := google.RetrieveSharedPhotos()
 	if err != nil {
@@ -28,3 +30,37 @@ func (r *Resolver) Photos(ctx context.Context) (*[]*photoResolver, error) {
 
 	return &photos, nil
 }
+
+
+// Resolver
+type photoResolver struct {
+	photo *model.Photo
+}
+
+func (r *photoResolver) ID() *graphql.ID {
+	id := graphql.ID(r.photo.ID)
+	return &id
+}
+
+func (r *photoResolver) Contributor() *string {
+	return &r.photo.Contributor
+}
+
+func (r *photoResolver) Urls() *[]*string {
+	urls := make([]*string, len(r.photo.Urls))
+
+	for i := range urls {
+		urls[i] = &r.photo.Urls[i]
+	}
+
+	return &urls
+}
+
+func (r *photoResolver) Timestamp() *string {
+	return &r.photo.Timestamp
+}
+
+func (r *photoResolver) Masked() *bool {
+	return &r.photo.Masked
+}
+
